188af718c6b761113642b7f0f17bfcc7d7af2026
TamNguyen <hoangtam.cntt@gmail.com>
2017-06-09 21:39:52 +0700

    Update mini chart

--- AccelerometerDemo/GraphViewController.swift
+++ AccelerometerDemo/GraphViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftChart
 
 @objc class GraphViewController: UIViewController {
 
--- AccelerometerDemo/TrackingViewController.m
+++ AccelerometerDemo/TrackingViewController.m
@@ -11,6 +11,7 @@
 #import "TrackingViewController.h"
 #import "AppController.h"
 #import "AccelerometerDemo-Swift.h"
+#import "EverChart.h"
 
 @interface TrackingViewController ()
 @property (weak, nonatomic) IBOutlet UIView *timerContainer;
@@ -18,18 +19,16 @@
 @property (weak, nonatomic) IBOutlet UIView *trackingContainer;
 @property (weak, nonatomic) IBOutlet UIButton *stopButton;
 @property (weak, nonatomic) IBOutlet UILabel *countingLabel;
-@property (weak, nonatomic) IBOutlet UIProgressView *xBar;
-@property (weak, nonatomic) IBOutlet UIProgressView *yBar;
-@property (weak, nonatomic) IBOutlet UIProgressView *zBar;
 @property (weak, nonatomic) IBOutlet UIView *graphContainer;
 @property (weak, nonatomic) IBOutlet UISwitch *acceleSwitch;
 @property (weak, nonatomic) IBOutlet UISwitch *gyroSwitch;
-@property (strong, nonatomic) MSRangeGauge *xGauge;
-@property (strong, nonatomic) MSRangeGauge *yGauge;
-@property (strong, nonatomic) MSRangeGauge *zGauge;
 @property (weak, nonatomic) IBOutlet UIView *xContainer;
 @property (weak, nonatomic) IBOutlet UIView *yContainer;
 @property (weak, nonatomic) IBOutlet UIView *zContainer;
+
+@property (strong, nonatomic) EverChart *chartX;
+@property (strong, nonatomic) EverChart *chartY;
+@property (strong, nonatomic) EverChart *chartZ;
 
 @end
 
@@ -60,28 +59,41 @@
     acceleMotionQueue = [[NSOperationQueue alloc] init];
     gyroMotionQueue = [[NSOperationQueue alloc] init];
     
-    _xGauge = [[MSRangeGauge alloc] initWithFrame:_xContainer.bounds];
-    _yGauge = [[MSRangeGauge alloc] initWithFrame:_yContainer.bounds];
-    _zGauge = [[MSRangeGauge alloc] initWithFrame:_zContainer.bounds];
-    
-    [_xContainer addSubview:_xGauge];
-    [_yContainer addSubview:_yGauge];
-    [_zContainer addSubview:_zGauge];
-    
-    for(MSRangeGauge* view in @[_xGauge,_yGauge,_zGauge]) {
-        view.minValue = 0;
-        view.maxValue = 100;
-        view.upperRangeValue = 80;
-        view.lowerRangeValue = 20;
-        view.value = 50;
-        view.rangeFillColor = [UIColor colorWithRed:.41 green:.76 blue:.73 alpha:1];
-    }
-    
     if( motionManager.isGyroAvailable ) {
         [motionManager startGyroUpdatesToQueue:gyroMotionQueue withHandler:^(CMGyroData * _Nullable gyroData, NSError * _Nullable error) {
             [[AppController shared] addGyro:gyroData.rotationRate];
         }];
     }
+    
+    [self addGraphView];
+}
+
+- (void) addGraphView {
+    
+    CGFloat width = MAX(self.xContainer.frame.size.width, self.xContainer.frame.size.height);
+    CGFloat height = MIN(self.xContainer.frame.size.width, self.xContainer.frame.size.height);
+    
+    self.chartX = [[EverChart alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+//    self.chartX.userInteractionEnabled = false;
+    [self.xContainer addSubview:self.chartX];
+    
+    self.chartY = [[EverChart alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+    self.chartY.userInteractionEnabled = false;
+    [self.yContainer addSubview:self.chartY];
+    
+    self.chartZ = [[EverChart alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+    self.chartZ.userInteractionEnabled = false;
+    [self.zContainer addSubview:self.chartZ];
+    
+    [self initMiniChart];
+    
+    [self renderChart];
+}
+
+- (void)viewDidLayoutSubviews {
+    self.chartX.frame = self.xContainer.bounds;
+    self.chartY.frame = self.yContainer.bounds;
+    self.chartZ.frame = self.zContainer.bounds;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -146,23 +158,160 @@
 - (void)startTrackingTimer  {
     [_timerContainer setHidden:YES];
     [_trackingContainer setHidden:NO];
-    trackingTimer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(trackingUpdate:) userInfo:nil repeats:YES];
+    trackingTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(trackingUpdate:) userInfo:nil repeats:YES];
 }
 
 - (void)trackingUpdate:(NSTimer*)timer {
-    trackedTime += 0.01;
+    trackedTime += 0.1;
     dispatch_async(dispatch_queue_create("com.nhn.trackingtimer", 0), ^{
        dispatch_async(dispatch_get_main_queue(), ^{
            [_countingLabel setText:[NSString stringWithFormat:@"%2.2f", trackedTime]];
            record = [[AppController shared] last];
-           [_xBar setProgress:fabs(record.x)];
-           [_yBar setProgress:fabs(record.y)];
-           [_zBar setProgress:fabs(record.z)];
-           [_xGauge setValue: (record.x*10)+50 animated:NO];
-           [_yGauge setValue: (record.y*10)+50 animated:NO];
-           [_zGauge setValue: (record.z*10)+50 animated:NO];
+           
+           [self renderChart];
        });
     });
+}
+
+-(void)initMiniChart{
+    
+    NSMutableArray *paddingx = [NSMutableArray arrayWithObjects:@"0",@"0",@"20",@"5",nil];
+    [self.chartX setPadding:paddingx];
+    
+    NSMutableArray *paddingy = [NSMutableArray arrayWithArray:paddingx];
+    [self.chartX setPadding:paddingy];
+    
+    NSMutableArray *paddingz = [NSMutableArray arrayWithArray:paddingx];
+    [self.chartX setPadding:paddingz];
+    
+    NSMutableArray *secsx = [[NSMutableArray alloc] init];
+    
+    [secsx addObject:@"1"];
+    
+    [self.chartX addSections:1 withRatios:secsx];
+    
+    [[[self.chartX sections] objectAtIndex:0] addYAxis:0];
+    
+    [self.chartX getYAxis:0 withIndex:0].tickInterval = 4;
+    
+    self.chartX.range = 241;
+    
+    NSMutableArray *secsy = [NSMutableArray arrayWithArray:secsx];
+    
+    [self.chartY addSections:1 withRatios:secsy];
+    
+    [[[self.chartY sections] objectAtIndex:0] addYAxis:0];
+    
+    [self.chartY getYAxis:0 withIndex:0].tickInterval = 4;
+    
+    self.chartY.range = 241;
+    
+    NSMutableArray *secsz = [NSMutableArray arrayWithArray:secsx];
+    
+    [self.chartZ addSections:1 withRatios:secsz];
+    
+    [[[self.chartZ sections] objectAtIndex:0] addYAxis:0];
+    
+    [self.chartZ getYAxis:0 withIndex:0].tickInterval = 4;
+    
+    self.chartZ.range = 241;
+    
+    NSMutableArray *series = [[NSMutableArray alloc] init];
+    NSMutableArray *secOne = [[NSMutableArray alloc] init];
+    
+    //    //均价
+    NSMutableDictionary *serie = [[NSMutableDictionary alloc] init];
+    NSMutableArray *data = [[NSMutableArray alloc] init];
+    
+    //实时价
+    serie = [[NSMutableDictionary alloc] init];
+    data = [[NSMutableArray alloc] init];
+    [serie setObject:kFenShiNowNameLine forKey:@"name"];
+    [serie setObject:@"数值" forKey:@"label"];
+    [serie setObject:data forKey:@"data"];
+    [serie setObject:kFenShiLine forKey:@"type"];
+    [serie setObject:@"1" forKey:@"yAxisType"];
+    [serie setObject:@"0" forKey:@"section"];
+    [serie setObject:kFenShiNowColor forKey:@"color"];
+    [series addObject:serie];
+    [secOne addObject:serie];
+    
+    [self.chartX setSeries:series];
+    [[[self.chartX sections] objectAtIndex:0] setSeries:secOne];
+    
+    NSMutableArray *seriesy = [NSMutableArray arrayWithArray:series];
+    NSMutableArray *secOney = [NSMutableArray arrayWithArray:secOne];
+    [self.chartY setSeries:seriesy];
+    [[[self.chartY sections] objectAtIndex:0] setSeries:secOney];
+    
+    NSMutableArray *seriesz = [NSMutableArray arrayWithArray:series];
+    NSMutableArray *secOnez = [NSMutableArray arrayWithArray:secOne];
+    [self.chartZ setSeries:seriesz];
+    [[[self.chartZ sections] objectAtIndex:0] setSeries:secOnez];
+}
+
+-(void)setOptions:(NSDictionary *)options ForSerie:(NSMutableDictionary *)serie {
+    [serie setObject:[options objectForKey:@"name"] forKey:@"name"];
+    [serie setObject:[options objectForKey:@"label"] forKey:@"label"];
+    [serie setObject:[options objectForKey:@"type"] forKey:@"type"];
+    [serie setObject:[options objectForKey:@"yAxis"] forKey:@"yAxis"];
+    [serie setObject:[options objectForKey:@"section"] forKey:@"section"];
+    [serie setObject:[options objectForKey:@"color"] forKey:@"color"];
+}
+
+- (void)renderChart {
+    
+    [self.chartX reset];
+    [self.chartX clearData];
+    [self.chartX clearCategory];
+    
+    [self.chartY reset];
+    [self.chartY clearData];
+    [self.chartY clearCategory];
+    
+    [self.chartZ reset];
+    [self.chartZ clearData];
+    [self.chartZ clearCategory];
+    
+    NSMutableArray *datax =[[NSMutableArray alloc] init];
+    NSMutableArray *datay =[[NSMutableArray alloc] init];
+    NSMutableArray *dataz =[[NSMutableArray alloc] init];
+    
+    NSMutableArray *categoryx =[[NSMutableArray alloc] init];
+    NSMutableArray *categoryy =[[NSMutableArray alloc] init];
+    NSMutableArray *categoryz =[[NSMutableArray alloc] init];
+    
+    NSArray *listArray = [[AppController shared] all];
+    
+    for (int i = 0;i<listArray.count;i++) {
+        
+        Acceleration *dic = listArray[i];
+        [categoryx addObject:dic.timetamp];
+        [categoryy addObject:dic.timetamp];
+        [categoryz addObject:dic.timetamp];
+        
+                NSArray *item1 = @[@(dic.data.x)];
+                [datax addObject:item1];
+        
+                item1 = @[@(dic.data.y)];
+                [datay addObject:item1];
+
+                item1 = @[@(dic.data.z)];
+                [dataz addObject:item1];
+    }
+    
+    [self.chartX appendToData:datax forName:kFenShiNowNameLine];
+    [self.chartX appendToCategory:categoryx forName:kFenShiNowNameLine];
+    
+    [self.chartY appendToData:datay forName:kFenShiNowNameLine];
+    [self.chartY appendToCategory:categoryy forName:kFenShiNowNameLine];
+    
+    [self.chartZ appendToData:dataz forName:kFenShiNowNameLine];
+    [self.chartZ appendToCategory:categoryz forName:kFenShiNowNameLine];
+    
+    [self.chartX setNeedsDisplay];
+    [self.chartY setNeedsDisplay];
+    [self.chartZ setNeedsDisplay];
 }
 
 - (IBAction)stopDidTouch:(id)sender {
